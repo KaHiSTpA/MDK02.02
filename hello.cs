0afa550a919ab864b0f5c60ebe66040dc3612904
KaHiSTpA <324537403+KaHiSTpA@users.noreply.github.com>
2026-09-07 00:40:46 +0900

    refactor: rename variable

--- hello.cs
+++ hello.cs
@@ -5,13 +5,13 @@ class Program
 {
     static void Main()
     {
+        var x = new Faker("ru");
+        
+        Console.WriteLine($"Имя: {x.Name.FullName()}");
+        Console.WriteLine($"Адрес: {x.Address.FullAddress()}");
         Console.WriteLine("Hello, World!");
         Console.WriteLine($"Сегодня: {DateTime.Now:d}");
         
         // TODO: Добавить проверку на пустой ввод
-        
-        var faker = new Faker("ru");
-        Console.WriteLine($"Имя: {faker.Name.FullName()}");
-        Console.WriteLine($"Адрес: {faker.Address.FullAddress()}");
     }
 }
