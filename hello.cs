d0260293071d144c71d848306366d75001cff52f
KaHiSTpA <324537403+KaHiSTpA@users.noreply.github.com>
2026-09-07 00:20:22 +0900

    feat: import library and generate random name and address

--- hello.cs
+++ hello.cs
@@ -1,4 +1,17 @@
-Console.WriteLine("Hello, World!");
-Console.WriteLine($"Сегодня: {DateTime.Now:d}");
+using System;
+using Bogus;
 
-// TODO: Добавить проверку на пустой ввод
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"Сегодня: {DateTime.Now:d}");
+        
+        // TODO: Добавить проверку на пустой ввод
+        
+        var faker = new Faker("ru");
+        Console.WriteLine($"Имя: {faker.Name.FullName()}");
+        Console.WriteLine($"Адрес: {faker.Address.FullAddress()}");
+    }
+}
